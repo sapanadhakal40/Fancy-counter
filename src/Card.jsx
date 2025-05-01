83a8432adab44dd3806fa9d9bc37f731d2ae3649
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-01 16:22:15 +0300

    Remove state and pass props from parent component(count, setcount,locked)

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,17 +2,35 @@ import Count from "./Count";
 import ResetButton from "./ResetButton";
 import Title from "./Title";
 import ButtonContainer from "./ButtonContainer";
-import { useState } from "react";
+import { useEffect } from "react";
 
-export default function Card() {
-  const [count, setCount] = useState(0);
+export default function Card({ locked, count, setCount }) {
+  // const [count, setCount] = useState(0);
   // const [locked, setLocked] = useState(0);
-  const locked = count === 5 ? true : false;
+  // const locked = count === 5 ? true : false;
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      if (event.code === "Space") {
+        const newCount = count + 1;
+        if (newCount > 5) {
+          setCount(5);
+          return;
+        }
+        setCount(newCount);
+      }
+    };
+    document.addEventListener("keydown", handleKeydown);
+    return () => {
+      document.removeEventListener("keydown", handleKeydown);
+    };
+  }, [count]);
+
   return (
     <div className={`card ${locked ? "card--limit" : ""}`}>
       <Title locked={locked} />
       <Count count={count} />
-      <ResetButton setCount={setCount} />
+      <ResetButton count={count} setCount={setCount} />
       <ButtonContainer setCount={setCount} locked={locked} />
     </div>
   );
