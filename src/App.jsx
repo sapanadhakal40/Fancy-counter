92c0e7c5e128a31b3c2cc09c2ffbeb0c8973c68c
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-01 16:14:12 +0300

    Move count state to parent and add main--limit class for locked state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Card from "./Card";
+import { useState } from "react";
 
 function App() {
+  // const locked = count === 5 ? true : false;
+  // const [locked, setLocked] = useState(5);
+
+  const [count, setCount] = useState(0);
+  const locked = count === 5 ? true : false;
   return (
-    <main>
-      <Card />
+    <main className={`${locked ? "main--limit" : ""}`}>
+      <Card locked={locked} setCount={setCount} count={count} />
     </main>
   );
 }
